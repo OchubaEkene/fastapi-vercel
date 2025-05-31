2564046a6610ef82b402af17fd15b5b03dc25528
Ekene Ochuba <alexanderochuba7@gmail.com>
2025-05-31 18:16:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-import os
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# Path to the favicon.ico file
-favicon_path = os.path.join(os.path.dirname(__file__), "static", "favicon.ico")
 
-# Mount the "static" directory
-app.mount("/static", StaticFiles(directory="static"), name="static")
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
 
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse(favicon_path)
+
+items = []
+
 
 @app.get("/")
-async def root():
+def root():
     return {"Hello": "World"}
+
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return items
+
+
+@app.get("/items", response_model=list[Item])
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
